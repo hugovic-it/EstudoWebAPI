e75472e2ddf95f4e21ee2a17843f7c5b269caf31
Hugo Victor <hugovic.it@gmail.com>
2022-07-25 13:07:43 -0300

    Model criado e pacotes EF instalados

---
+++ Models/Usuario.cs
@@ -0,0 +1,14 @@
+using System.Security.AccessControl;
+namespace NDogs.Models
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+         
+        
+        
+        
+    }
+}
