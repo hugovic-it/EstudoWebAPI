2d6aba0a7b0843ded9aa12de45304390ebe2c430
Hugo Victor <hugovic.it@gmail.com>
2022-07-27 00:22:41 -0300

    CRUD inserido no UsuariosController

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -27,7 +27,60 @@ namespace NDogs.Controllers
             return Ok(usuarios);
         }
 
+        [HttpGet("{id:int}", Name = "ObterUsuario")]
+        public ActionResult<Usuario> Get(int id)
+        {//Retorna o usuario pelo Id
+            var usuario = _context.Usuarios.FirstOrDefault(p => p.UsuarioId == id);
+            if (usuario is null)
+            {
+                return NotFound();
+            }
+            return usuario;
+        }
 
+        [HttpPost]
+        public ActionResult Post(Usuario usuario)
+        {
+
+            if (usuario is null)
+            {
+                return BadRequest();
+            }
+
+            _context.Usuarios.Add(usuario);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterUsuario",
+                 new { id = usuario.UsuarioId }, usuario);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Usuario usuario)
+        {
+            if (id != usuario.UsuarioId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(usuario).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var usuario = _context.Usuarios.FirstOrDefault(p => p.UsuarioId == id);
+            //var produto = _context.Usuarios.Find(id)
+            if (usuario is null)
+            {
+                return NotFound("Produto nao localizado...");
+            }
+            _context.Usuarios.Remove(usuario);
+            _context.SaveChanges();
+
+            return Ok(usuario);
+        }
 
     }
 }
